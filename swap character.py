ef3c48c52d2c48aed6f3919488bba461cf196170
SNVSKOMAL <21bq1a4208@vvit.net>
2023-11-26 09:38:26 +0530

    enter input --> abc enter input --> acb here we are changing the only the adjacent string if they are changed correctly then print yes else no

---
+++ swap character.py
@@ -0,0 +1,19 @@
+a = input()
+b = input()
+#abcd
+#abdc
+
+list_1 = list(a)
+list_2 = list(b)
+
+length = len(list_1)
+for i in range(length):
+    if list_1[i] != list_2[i]:
+        temp = list_2[i+1]
+        list_2[i+1] = list_2[i]
+        list_2[i] = temp
+        break
+if list_1 == list_2:
+    print("Yes")
+else:
+    print("No")

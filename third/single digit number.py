631baedd886113b9e8d5cd4759105b4d8e92960d
SNVSKOMAL <21bq1a4208@vvit.net>
2023-12-03 15:13:54 +0530

    explanation is given in the code with comments

---
+++ third/single digit number.py
@@ -0,0 +1,21 @@
+#---<><><><><><><<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><--
+#You have given a positive number N and asked to convert that
+#number N into a single digit by repeatedly adding its digits until it
+#becomes a single digit.
+#Repeatedly add the digits of the given number 545 until it
+#becomes a single digit like as shown below
+#Add the digits of the given number:
+#Repeat the process and add the digits of the result:
+#5 + 4 + 5 = 14
+#1 + 4 = 5
+#---<><><><><><><<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><--
+
+
+n = str(int(input()))
+m = len(n)
+while m > 1:
+    string = list(n)
+    num = sum(list(map(int,string())))
+    n = str(num)
+    m= len(n)
+print(int(n))

73b015bf7f359995137494d2944817e248ec89c9
SNVSKOMAL <21bq1a4208@vvit.net>
2023-12-04 13:25:30 +0530

    explanation is given in the code with comments

---
+++ fourth/Missing number.py
@@ -0,0 +1,12 @@
+##---<><><><><><><<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><--
+#here the input is given like integers in that some are missing
+#and the output should be like the missing integers
+##---<><><><><><><<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><--
+
+s = list(map(int, input().split()))
+num = sorted(s,reverse=True)
+n = len(s)
+for i in range(-1,-n-1,-1):
+    if i not in s:
+        print(i)
+        break

eeedba00bd0e82b1fab8b008e080b1676368004a
SNVSKOMAL <21bq1a4208@vvit.net>
2024-03-18 05:59:44 +0530

    practise-1

--- Practise/p_2.py
+++ Practise/p_2.py
@@ -1,6 +1,6 @@
 def main():
     string = input("Enter a string: ")
-    upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    upper_case = "abcdefghijklmnopqrstuvwxyz"
     is_valid = all(char in upper_case or char == '_' for char in string)
 
     if is_valid:

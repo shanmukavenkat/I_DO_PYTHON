e8592e1b03967b33b862961029e6185b6407fce1
S.N.V.S.KOMAL <113082454+shanmukavenkat@users.noreply.github.com>
2024-06-13 05:38:56 +0530

    practise-1

---
+++ Practise/edge_of_a_triangle.py
@@ -0,0 +1,6 @@
+def edge_of_a_triangle(side1,side2):
+    return (side1 + side2) - 1
+
+user_input = int(input())
+user_input2 = int(input())
+print(edge_of_a_triangle(user_input,user_input2),"is the edge of the triangle.")
